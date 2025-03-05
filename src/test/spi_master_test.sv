2118cbdf2aa4399a2d84d2e7eba2034c7dbbf98a
Victor Krushinsky <vittorio.kru@yandex.ru>
2025-03-05 11:29:04 +0300

    feat: add phases in env

--- src/test/spi_master_test.sv
+++ src/test/spi_master_test.sv
@@ -1,8 +1,31 @@
 class spi_master_test extends uvm_test;
     `uvm_component_utils (spi_master_test);
 
-    
-    function new();
+    spi_master_env_base env;
+
+    function new(string name = "", uvm_component parent = null);
+        super.new(name, parent);
+    endfunction
+
+    virtual function void build_phase(uvm_phase phase);
+        uvm_resource_db#(int)::set(
+           "*", "item_amount", $urandom_range(100, 200), this
+        );
+        env = spi_master_env_base::type_id::create("env", this);
+    endfunction
+
+    virtual task main_phase(uvm_phase phase);
+        spi_master_seq_base seq;
+        phase.raise_objection(this);
+
+        // Base sequence
+        seq = spi_master_seq_base::type_id::create("seq");
+        seq.start(env.m_agent.seqr);
+
+        // I can create a different sequences
+        // seq = spi_master_seq_another::type_id::create("seq_another");
+        // seq.start(env.m_agent.seqr);
         
-    endfunction //new()
+        phase.drop_objection(this);
+    endtask
 endclass //spi_master_test
